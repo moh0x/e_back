03f5152df8887299c9e2b45df1ceea11d02d0036
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-03 10:01:20 -0700

    Update items_controller.js

--- controllers/home/items_controller.js
+++ controllers/home/items_controller.js
@@ -65,6 +65,66 @@ const getLatestItems = async(req,res)=>{
     res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
    }
 }
+const getLatestItemsVerifyAdmin = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    const limit = 15;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const items = await Item.find({itemActive:true}).sort({itemDate:-1}).limit(limit).skip(skip);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+const getLatestItemsNotVerifyAdmin = async(req,res)=>{
+   try {
+    var token =  req.headers.token;
+    const limit = 15;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const items = await Item.find({itemActive:false}).sort({itemDate:-1}).limit(limit).skip(skip);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":items});
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+const changeItemStatusAdmin = async(req,res)=>{
+   try {
+      const itemId = req.body.itemId;
+      const itemSer = await Item.findById(itemId);
+    if (itemSer) {
+      const active = itemSer.itemActive;
+      const itemSerActive = !active;
+    const item = await Item.findByIdAndUpdate(itemId,{
+      $set:{
+         itemActive:itemSerActive
+      }
+    });
+    await item.save();
+    const itemRet = await Item.findById(itemId);
+    res.status(200).json({"status":httpsStatus.SUCCESS,"data":itemRet});
+    } else {
+      res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no item"});
+    }
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+const deleteItemAdmin = async(req,res)=>{
+   try {
+      const itemId = req.body.itemId;
+      const itemSer = await Item.findById(itemId);
+    if (itemSer) {
+    const item = await Item.findByIdAndDelete(itemId);
+    res.status(200).json({"status":httpsStatus.SUCCESS});
+    } else {
+      res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no item"});
+    }
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
  module.exports = {
-    getAllItems,getSearchItems,getLikesItems,getCartItems,getLatestItems
+    getAllItems,getSearchItems,getLikesItems,getCartItems,getLatestItems,deleteItemAdmin,changeItemStatusAdmin,getLatestItemsNotVerifyAdmin,getLatestItemsVerifyAdmin
    }
