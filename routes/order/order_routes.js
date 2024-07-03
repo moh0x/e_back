724205a69ff075b033c6db45915fc8e80e174a0a
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-03 02:18:48 -0700

    Update order_routes.js

--- routes/order/order_routes.js
+++ routes/order/order_routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../../controllers/order/order_controller');
 const { verifyToken } = require("../../utility/verify_token");
+const { verifyAdmin } = require('../../utility/verify_role_admin');
 router.get('/myOrders',verifyToken,orderController.getMyOrders);
+router.get('/mySummary',verifyToken,verifyAdmin,orderController.getMySummary);
 router.post('/addOrder',verifyToken,orderController.addOrder);
 router.delete('/deleteOrder',verifyToken,orderController.deleteOrder);
   module.exports = 
