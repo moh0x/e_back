f54c6cc2ff6f41578ecfb88fff115e8fde180a14
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-04 06:00:00 -0700

    Update deliviry_routes.js

--- routes/deliviry/deliviry_routes.js
+++ routes/deliviry/deliviry_routes.js
@@ -4,6 +4,7 @@ const deliviryControoler = require('../../controllers/deliviy/deliviry_controlle
 const { verifyToken } = require("../../utility/verify_token");
 const {body,validationResult } = require("express-validator");
 const { verifyDeliviry } = require('../../utility/verify_role_deliviry');
+const { verifyAdmin } = require('../../utility/verify_role_admin');
 router.post('/register',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),body("phone").isMobilePhone().isLength({min:10,max:15}).withMessage("type valid phone number"),body("userName").isLength({min:6,max:30}).withMessage("type valid user name"),deliviryControoler.registerFunc);
 
 router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,verifyDeliviry,deliviryControoler.confirmAccountFunc);
@@ -12,7 +13,11 @@ router.patch('/resetPassword',body("resetPasswordCode").isString().isLength({min
 
 router.post('/login',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),deliviryControoler.loginFunc);
   
-router.post('/sendResetCode',deliviryControoler.sendResetCodeFunc)
+router.post('/sendResetCode',deliviryControoler.sendResetCodeFunc);
+router.get('/admin/getAllDeliviriesAgreeAdmin',verifyToken,verifyAdmin,deliviryControoler.getAllDeliviriesAgreeAdmin);
+router.get('/admin/getAllDeliviriesNotAgreeAdmin',verifyToken,verifyAdmin,deliviryControoler.getAllDeliviriesNotAgreeAdmin);
+router.patch('/admin/changeStatusAgree',verifyToken,verifyAdmin,deliviryControoler.changeDeliviryStatusAdmin);
+router.delete('/admin/deleteDeliviry',verifyToken,verifyAdmin,deliviryControoler.deleteDeliviryAdmin);
   module.exports = 
     router
   
