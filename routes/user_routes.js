e192cfd4a9f20c5cfd0a547cf21957bfb9a4b2e7
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-04 06:03:59 -0700

    Update user_routes.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -4,6 +4,7 @@ const userControoler = require('../controllers/user_controllers');
 const { verifyToken } = require("../utility/verify_token");
 const {body,validationResult } = require("express-validator");
 const { verifyUser } = require('../utility/verify_role_user');
+const { verifyAdmin } = require('../utility/verify_role_admin');
 router.post('/register',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),body("phone").isMobilePhone().isLength({min:10,max:15}).withMessage("type valid phone number"),body("userName").isLength({min:6,max:30}).withMessage("type valid user name"),userControoler.registerFunc);
 
 router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,verifyUser,userControoler.confirmAccountFunc);
@@ -12,7 +13,9 @@ router.patch('/resetPassword',body("resetPasswordCode").isString().isLength({min
 
 router.post('/login',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),userControoler.loginFunc);
   
-router.post('/sendResetCode',userControoler.sendResetCodeFunc)
+router.post('/sendResetCode',userControoler.sendResetCodeFunc);
+router.get('/admin/getAllUsers',verifyToken,verifyAdmin,userControoler.getAllUsersAdmin)
+router.delete('/admin/deleteUser',verifyToken,verifyAdmin,userControoler.deleteUserAdmin)
   module.exports = 
     router
   
