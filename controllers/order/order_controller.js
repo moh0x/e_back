d0f8a4564f96c1d4a285f18c13e50bffad4884b8
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-04 15:18:32 -0700

    Update order_controller.js

--- controllers/order/order_controller.js
+++ controllers/order/order_controller.js
@@ -1,8 +1,10 @@
 const httpsStatus = require('../../constants/https_status');
 const { Adress } = require('../../models/adress/adress_model');
+const { Deliviry } = require('../../models/deliviry/deliviry_model');
 const { Item } = require('../../models/home/items_model');
 const { Order } = require('../../models/order/order_model');
 const { User } = require('../../models/user_model');
+const { Vendor } = require('../../models/vendor/vendor_model');
 const addOrder = async(req,res)=>{
     try {
      var token =  req.headers.token;
@@ -17,13 +19,15 @@ const addOrder = async(req,res)=>{
   if (adressesIds.includes(req.body.orderAdress)) {
     for (let index = 0; index < user.cart.length; index++) {
         const item = user.cart[index]['itemId'];
+        const product = await Item.findById(item);
         const order = new Order({
             orderAdress:req.body.orderAdress,
             orderCount:user.cart[index]['count'],
             orderDetails:user.cart[index]['details'],
             orderFirstDate:Date.now(),
             orderlastDate:Date('0000000000'),
-            orderStatusId:"first",
+            orderPrice:product.itemDisCount == 0 ? product.itemPrice * user.cart[index]['count'] : product.itemNewPrice * user.cart[index]['count'],
+            orderStatusId:"order by user",
             orderUserId:user._id,
             orderProductId:item,
             orderShiping:user.cart[index]['count'] * 500
@@ -85,41 +89,43 @@ const addOrder = async(req,res)=>{
      res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
     }
  }
-const getMySummary = async(req,res)=>{
+ const getMySummary = async(req,res)=>{
    try {
     var token =  req.headers.token;
-    const orders = await Order.find({orderStatusId:"finished"}); 
+    const deliviries = await Deliviry.find({isAgree:true}); 
+    const vendors = await Vendor.find({isAgree:true}); 
     var orderRet = [];
     var all = 0;
-    var allWithoutShiping = 0 ;
+    var sales = 0 ;
     var shipping = 0;
     var myFreeShipping = 0;
-    var myFreeallWithoutShiping = 0;
+    var myFreesales = 0;
     var shippingMoney = 0;
-    var allWithoutShipingMoney = 0;
+    var salesMoney = 0;
     var myAllFree = 0;
     var allMoney = 0;
-    for (let index = 0; index < orders.length; index++) {
-        allWithoutShiping =  allWithoutShiping + orders[index].orderPrice;
-        shipping = shipping + orders[index].orderShiping;
-           
+    for (let index = 0; index < deliviries.length; index++) {
+        shipping = shipping + deliviries[index].shipping;
+        myFreeShipping = myFreeShipping + deliviries[index].shippingTax;
+        shippingMoney = shippingMoney + deliviries[index].myFreeShipping        
     }
-    all = allWithoutShiping + shipping;
-    myFreeShipping = shipping * (10/100);
-    myFreeallWithoutShiping = allWithoutShiping * (14/100);
-    shippingMoney = shipping - myFreeShipping;
-    allWithoutShipingMoney = allWithoutShiping - myFreeallWithoutShiping;
-    myAllFree = myFreeallWithoutShiping + myFreeShipping;
-    allMoney = all - myAllFree;
+    for (let index = 0; index < vendors.length; index++) {
+      sales = sales + vendors[index].sales;
+      myFreesales = myFreesales + vendors[index].salesTax;
+      salesMoney = salesMoney + vendors[index].myFreeSales        
+  }
+    all = sales + shipping;
+    allMoney = salesMoney - shippingMoney;
+    myAllFree = myFreesales + myFreeShipping
        res.status(200).json({"status":httpsStatus.SUCCESS,"data":{
         "all":all,
-        "vendor":allWithoutShiping,
+        "sales":sales,
         "shipping":shipping,
-        "vendorMoney":allWithoutShipingMoney,
+        "salesMoney":salesMoney,
         "shippingMoney":shippingMoney,
         "allMoney":allMoney,
-        "myEarnVendor":myFreeallWithoutShiping,
-        "myEarnShipping":myFreeShipping,
+        "myFreeSales":myFreesales,
+        "myFreeShipping":myFreeShipping,
         "myAllFree":myAllFree
 
        }}); 
@@ -129,6 +135,31 @@ const getMySummary = async(req,res)=>{
    }
 
   }
+  const getDelivirySummary = async(req,res)=>{
+    try {
+     var token =  req.headers.token;
+     const deliviry = await Deliviry.findOne({token:token});
+     const orders = await Order.find({orderStatusId:"finished",orderDeliviryId:deliviry.id}); 
+     var orderRet = [];
+     var shipping = 0;
+     var myFreeShipping = 0;
+     var shippingTax = 0;
+     for (let index = 0; index < orders.length; index++) {
+         shipping = shipping + orders[index].orderShiping       
+     }
+    shippingTax = shipping * (14/100);
+    myFreeShipping = shipping - shippingTax;
+        res.status(200).json({"status":httpsStatus.SUCCESS,"data":{
+         "myEarn":shipping,
+         "myTax":shippingTax,
+         "myFreeEarn":myFreeShipping
+ 
+        }}); 
+    } catch (error){
+      console.log(error);
+     res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+    }}
+
  module.exports = {
-    getMyOrders,addOrder,deleteOrder,getMySummary
+    getMyOrders,addOrder,deleteOrder,getDelivirySummary,getMySummary
    }
