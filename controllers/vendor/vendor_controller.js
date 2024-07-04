db58fbb44398df936dfe04ae7fc013dd2742b3e0
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-04 05:55:11 -0700

    Update vendor_controller.js

--- controllers/vendor/vendor_controller.js
+++ controllers/vendor/vendor_controller.js
@@ -1,22 +1,319 @@
-const express = require('express');
-const router = express.Router();
-const vendorControoler = require('../../controllers/vendor/vendor_controller');
-const { verifyToken } = require("../../utility/verify_token");
-const {body,validationResult } = require("express-validator");
-const { verifyVendor } = require('../../utility/verify_role_vendor');
-const { verifyAdmin } = require('../../utility/verify_role_admin');
-router.post('/register',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),body("phone").isMobilePhone().isLength({min:10,max:15}).withMessage("type valid phone number"),body("userName").isLength({min:6,max:30}).withMessage("type valid user name"),vendorControoler.registerFunc);
-router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,verifyVendor,vendorControoler.confirmAccountFunc);
-router.get('/vendorInfo',verifyToken,verifyVendor,vendorControoler.getVendorInfo);
-router.patch('/resetPassword',body("resetPasswordCode").isString().isLength({min:1,max:5}),body('password').isString().isLength({min:8,max:30}),vendorControoler.resetPasswordFunc);
+const { Vendor } = require("../../models/vendor/vendor_model");
 
-router.post('/login',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),vendorControoler.loginFunc);
+const gen = require("@codedipper/random-code");
+var jwt = require('jsonwebtoken');
+const { CourierClient } = require("@trycourier/courier");
+const bcrypt = require("bcryptjs");
+const {body,validationResult } = require("express-validator");
+const httpsStatus = require('../../constants/https_status');
+const { Order } = require("../../models/order/order_model");
+const getVendorInfo = async(req,res)=>{
+  try {
+   const token = req.headers.token;
+   const vendor = await Vendor.findOne({token:token},{__v:false,password:false});  
+       res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendor});
+   
+  } catch (error) {
+   res.status(400).json({"status":httpsStatus.ERROR,"message":"error"});
+  }
+}
+const registerFunc = async(req,res)=>{
+    try {
+     const email = await Vendor.findOne({email : req.body.email});
+     const valid = validationResult (req)
+    if (valid.isEmpty()) {
+     if (!email) {
+         const token = jwt.sign({ email: req.body.email,password:req.body.password }, 'shhhhh');
+         const verifyCode = gen(5,"0123456789");
+         var password =await  bcrypt.hash(req.body.password,10)
+         const vendor =  new Vendor({
+             userName:req.body.userName,
+             token:token,
+             email:req.body.email,
+             password:password,
+             phone:req.body.phone,
+             verifyCode:verifyCode,
+     resetPasswordCode:0,
+     isAgree:false,
+             
+             isVerify:false,
+     });
+         await vendor.save();
+         const newVendor= await Vendor.findOne({email : req.body.email},{__v:false,password:false});
+       const courier = new CourierClient(
+         { authorizationToken: "pk_prod_5T2N91YKAJ4FKGH0YAM3X4NKRB0V"});
+ 
+       const { requestId } =  courier.send({
+         message: {
+           content: {
+             title: "confirm your email",
+             body: `your verification code is ${verifyCode}`
+           },
+           to: {
+             email: `${newVendor.email}`
+           }
+         }
+       });
+         res.status(200).json({"status":httpsStatus.SUCCESS,"data":newVendor});
+        } else {
+         res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"user already exist"});
+        }
+    }
+    else {
+     res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"check your input"});
+    }
+    } catch (error) {
+        console.log(error);
+     res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+     
+    }
+ }
+ const loginFunc = async(req,res)=>{
+   
+  try {
+     const vendor = await Vendor.findOne({email : req.body.email},{__v:false});
+  const valid = validationResult (req);
+  const passwordMatch = await bcrypt.compare(req.body.password,vendor.password);
   
-router.post('/sendResetCode',vendorControoler.sendResetCodeFunc);
-router.get('/admin/getAllVendorsAgreeAdmin',verifyToken,verifyAdmin,vendorControoler.getAllVendorsAgreeAdmin);
-router.get('/admin/getAllVendorsNotAgreeAdmin',verifyToken,verifyAdmin,vendorControoler.getAllVendorsNotAgreeAdmin);
-router.patch('/admin/changeStatusAgree',verifyToken,verifyAdmin,vendorControoler.changeVendorStatusAdmin);
-router.delete('/admin/deleteVendors',verifyToken,verifyAdmin,vendorControoler.deletevendorAdmin);
-  module.exports = 
-    router
-  
+if (valid.isEmpty()) {
+ if (vendor) {
+     if (passwordMatch == true) {
+         if (vendor.isVerify) {
+             const vendorRet = await Vendor.findOne({email : req.body.email},{__v:false,password:false});
+             res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendorRet});
+         } else {
+             const vendorRet = await Vendor.findOne({email : req.body.email},{__v:false,password:false});
+             const verifyCode = gen(5,"0123456789");
+            await Vendor.findByIdAndUpdate(vendorRet._id,{
+                 $set:{
+                     verifyCode:verifyCode
+                 }
+             })
+             
+             const courier = new CourierClient(
+                 { authorizationToken: "pk_prod_5T2N91YKAJ4FKGH0YAM3X4NKRB0V"});
+               const { requestId } =  courier.send({
+                 message: {
+                   content: {
+                     title: "confirm your email",
+                     body: `your verification code is ${verifyCode}`
+                   },
+                   to: {
+                     email: `${vendorRet.email}`
+                   }
+                 }
+               });
+               res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendorRet});
+         }
+     } else {
+         res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"password not match"});
+     }
+    } else {
+     res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"there is no user with this email"});
+    }
+} else {
+ res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"check your input"});
+}
+  } catch (error) {
+     res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+  }
+
+}
+const sendResetCodeFunc = async(req,res)=>{
+  try {
+   const vendor = await Vendor.findOne({email:req.body.email},{__v:false,password:false,token:false});
+   if (vendor) {
+               const resetPasswordCode = gen(5,"0123456789");
+           await    Vendor.findByIdAndUpdate(vendor._id,{
+                   $set:{
+                       resetPasswordCode:resetPasswordCode
+                   }
+               });    
+               const courier = new CourierClient(
+                   { authorizationToken: "pk_prod_5T2N91YKAJ4FKGH0YAM3X4NKRB0V"});
+                 const { requestId } =  courier.send({
+                   message: {
+                     content: {
+                       title: "reset password",
+                       body: `your reset code is ${resetPasswordCode}`
+                     },
+                     to: {
+                       email: `${vendor.email}`
+                     }
+                   }
+                 });
+                
+                 res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendor});
+   } else {
+       res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"we don't have user with this email"});
+   }
+  } catch (error) {
+   res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+  }
+}
+const resetPasswordFunc = async(req,res)=>{
+  try {     
+      const email = req.body.email;
+      const vendor = await Vendor.findOne({email:email},{__v:false,password:false,token:false});
+      const resetPasswordCode = req.body.resetPasswordCode;
+      const password =await bcrypt.hash(req.body.password,10);
+if (vendor) {
+if (resetPasswordCode == vendor.resetPasswordCode && vendor.resetPasswordCode != 0 ) {
+  await Vendor.findByIdAndUpdate(vendor._id,{
+      $set:{
+          isVerify:true,
+          verifyCode:0,
+          resetPasswordCode:0,
+          password:password
+      }
+  });
+  res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendor});
+} else {
+  res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"verification code not match"});
+}
+} else {
+  res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no user"});
+}
+ 
+  } catch (error) {
+
+      res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   
+  }
+}
+const confirmAccountFunc = async(req,res)=>{
+  try {     
+      const token = req.headers.token;
+      const vendor = await Vendor.findOne({token:token},{__v:false,password:false,token:false});
+      const verifyCode = req.body.verifyCode;
+if (vendor) {
+if (verifyCode == vendor.verifyCode && vendor.verifyCode != 0 ) {
+  await Vendor.findByIdAndUpdate(vendor._id,{
+      $set:{
+          isVerify:true,
+          verifyCode:0,
+          resetPasswordCode:0,
+      }
+  });
+  res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendor});
+} else {
+  res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"verification code not match"});
+}
+} else {
+  res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no user"});
+}
+ 
+  } catch (error) {
+      res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   
+  }
+}
+const getAllVendorsAgreeAdmin =  async(req,res)=>{
+  try {
+    const limit = 15;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const vendors = await Vendor.find({isAgree:true}).limit(limit).skip(skip);
+    const vendorsIds = [];
+    for (let index = 0; index < vendors.length; index++) {
+      vendorsIds.unshift(vendors[index].id);  
+    }
+    const orders = await Order.find({orderStatusId:"finished",orderVendorId:{$in:vendorsIds}}); 
+    var sale = 0;
+    var myFreesale = 0;
+    var saleTax = 0;
+    const result = [];
+    for (let index = 0; index < vendors.length; index++) {
+        for (let ind = 0; ind < orders.length; ind++) {
+            sale = sale + orders[ind].orderPrice;       
+        }
+       saleTax = sale * (14/100);
+       myFreesale = sale - saleTax;
+        result.unshift({
+            "vendor":vendors[index],
+            "myEarn":sale.toFixed(),
+            "myTax":saleTax.toFixed(),
+            "myFreeEarn":myFreesale.toFixed()
+        })
+        
+    }
+   res.status(200).json({"status":httpsStatus.SUCCESS,"data":result}); 
+   } catch (error) {
+    console.log(error);
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"
+    }); 
+   }
+}
+const getAllVendorsNotAgreeAdmin =  async(req,res)=>{
+   try {
+    const limit = 15;
+    const page = req.body.page || 1;
+    const skip = (page - 1) * limit;
+    const vendors = await Vendor.find({isAgree:false}).limit(limit).skip(skip);
+    const vendorsIds = [];
+    for (let index = 0; index < vendors.length; index++) {
+      vendorsIds.unshift(vendors[index].id);  
+    }
+    const orders = await Order.find({orderStatusId:"finished",orderVendorId:{$in:vendorsIds}}); 
+    var sale = 0;
+    var myFreesale = 0;
+    var saleTax = 0;
+    const result = [];
+    for (let index = 0; index < vendors.length; index++) {
+        for (let ind = 0; ind < orders.length; ind++) {
+            sale = sale + orders[ind].orderPrice;       
+        }
+       saleTax = sale * (14/100);
+       myFreesale = sale - saleTax;
+        result.unshift({
+            "vendor":vendors[index],
+            "myEarn":sale.toFixed(),
+            "myTax":saleTax.toFixed(),
+            "myFreeEarn":myFreesale.toFixed()
+        })
+        
+    }
+   res.status(200).json({"status":httpsStatus.SUCCESS,"data":result}); 
+   } catch (error) {
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"
+    }); 
+   }
+}
+const changeVendorStatusAdmin = async(req,res)=>{  try {
+   const vendorId = req.body.vendorId;
+   const vendorSer = await Vendor.findById(vendorId);
+ if (vendorSer) {
+   const agree = vendorSer.isAgree;
+   const vendorSerAgree = !agree;
+ const vendor = await Vendor.findByIdAndUpdate(vendorId,{
+   $set:{
+      isAgree:vendorSerAgree
+   }
+ });
+ await vendor.save();
+ const vendorRet = await Vendor.findById(vendorId);
+ res.status(200).json({"status":httpsStatus.SUCCESS,"data":vendorRet});
+ } else {
+   res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no user"});
+ }
+} catch (error){
+console.log(error);
+ res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+}}
+const deletevendorAdmin = async(req,res)=>{
+   try {
+      const vendorId = req.body.vendorId;
+      const vendorSer = await Vendor.findById(vendorId);
+    if (vendorSer) {
+    const vendor = await Vendor.findByIdAndDelete(vendorId);
+    res.status(200).json({"status":httpsStatus.SUCCESS});
+    } else {
+      res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"no user"});
+    }
+   } catch (error){
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
+   }
+}
+ module.exports = {
+  registerFunc,loginFunc,sendResetCodeFunc,resetPasswordFunc,confirmAccountFunc,getVendorInfo,deletevendorAdmin,changeVendorStatusAdmin,getAllVendorsAgreeAdmin,getAllVendorsNotAgreeAdmin
+ }
